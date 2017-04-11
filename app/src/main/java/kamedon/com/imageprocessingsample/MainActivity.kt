d3d8a415f333f4b181c8c65fffe81099074c152e
Kamei Hidetoshi <kamei.hidetoshi@classmethod.jp>
2017-04-11 21:15:38 +0900

    ページ移動を実装

--- app/src/main/java/kamedon/com/imageprocessingsample/MainActivity.kt
+++ app/src/main/java/kamedon/com/imageprocessingsample/MainActivity.kt
@@ -1,5 +1,6 @@
 package kamedon.com.imageprocessingsample
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById(R.id.btnRotation).setOnClickListener {
+            startActivity(Intent(applicationContext, RotationActivity::class.java))
+        }
+        findViewById(R.id.btnTranslation).setOnClickListener {
+            startActivity(Intent(applicationContext, TranslationActivity::class.java))
+        }
     }
 }
