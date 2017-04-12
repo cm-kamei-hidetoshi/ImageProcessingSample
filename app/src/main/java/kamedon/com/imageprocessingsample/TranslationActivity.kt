068f98f4dda2ddae1e012863b7a1e18c0ac768c4
Kamei Hidetoshi <kamei.hidetoshi@classmethod.jp>
2017-04-12 09:12:34 +0900

    画像の移動を実装

--- app/src/main/java/kamedon/com/imageprocessingsample/TranslationActivity.kt
+++ app/src/main/java/kamedon/com/imageprocessingsample/TranslationActivity.kt
@@ -1,17 +1,35 @@
 package kamedon.com.imageprocessingsample
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.BitmapCompat
+import android.view.MotionEvent
 import android.view.SurfaceView
+import kamedon.com.imageprocessingsample.view.ImageTranslationView
 
 class TranslationActivity : AppCompatActivity() {
 
     val surfaceView by lazy {
-        findViewById(R.id.surfaceView) as SurfaceView
+        findViewById(R.id.surfaceView) as ImageTranslationView
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_translation)
+        surfaceView.setup(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+        surfaceView.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    surfaceView.translate(event.x, event.y)
+                    return@setOnTouchListener true
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    surfaceView.translate(event.x, event.y)
+                }
+            }
+            false
+
+        }
     }
 }
