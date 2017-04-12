b633b3e1cac39c55e3a62cdd917476c8bd68716b
Kamei Hidetoshi <kamei.hidetoshi@classmethod.jp>
2017-04-12 09:30:32 +0900

    点と円の当たり判定を作成

--- app/src/main/java/kamedon/com/imageprocessingsample/CollisionActivity.kt
+++ app/src/main/java/kamedon/com/imageprocessingsample/CollisionActivity.kt
@@ -2,11 +2,30 @@ package kamedon.com.imageprocessingsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import kamedon.com.imageprocessingsample.view.ImageCollisionView
 
 class CollisionActivity : AppCompatActivity() {
+
+    val surfaceView by lazy {
+        findViewById(R.id.surfaceView) as ImageCollisionView
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_collision)
+        surfaceView.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    surfaceView.collide(event.x, event.y)
+                    return@setOnTouchListener true
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    surfaceView.collide(event.x, event.y)
+                }
+            }
+            false
+
+        }
     }
 }
