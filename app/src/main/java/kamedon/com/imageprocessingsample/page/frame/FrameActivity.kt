eb63a6cf7316ef3198e001e86226a416a0722ed1
Kamei Hidetoshi <kamei.hidetoshi@classmethod.jp>
2017-04-16 12:01:08 +0900

    Rxをインストール

--- app/src/main/java/kamedon/com/imageprocessingsample/page/frame/FrameActivity.kt
+++ app/src/main/java/kamedon/com/imageprocessingsample/page/frame/FrameActivity.kt
@@ -13,5 +13,6 @@ class FrameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frame)
+
     }
 }
