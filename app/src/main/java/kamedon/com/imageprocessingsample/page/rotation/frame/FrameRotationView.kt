f9cd9a807be3763797503fe67cec73e1e43578c2
Kamei Hidetoshi <kamei.hidetoshi@classmethod.jp>
2017-04-20 23:36:51 +0900

    コメントを追記

--- app/src/main/java/kamedon/com/imageprocessingsample/page/rotation/frame/FrameRotationView.kt
+++ app/src/main/java/kamedon/com/imageprocessingsample/page/rotation/frame/FrameRotationView.kt
@@ -128,7 +128,9 @@ class FrameRotationView @JvmOverloads constructor(
 
     }
 
+    //xを回転
     fun fx(x: Float, y: Float, rad: Double) = x * Math.cos(rad) - y * Math.sin(rad)
 
+    //yを回転
     fun fy(x: Float, y: Float, rad: Double) = x * Math.sin(rad) + y * Math.cos(rad)
 }
